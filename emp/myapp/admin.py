6ed8552e061d28132cc3597549370c41cb250e8f
kiwi965 <kevinbenny2002@gmail.com>
2023-12-14 21:04:25 +0530

    emp

--- emp/myapp/admin.py
+++ emp/myapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import emp
+
+admin.site.register(emp)
+
+
+from .models import manager
+
+admin.site.register(manager)
+
+from .models import login
+
+admin.site.register(login)
+
+
--- emp/myapp/models.py
+++ emp/myapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class emp(models.Model):
+  empid=models.CharField(max_length=100,unique=True)
+  empname=models.CharField(max_length=150,null=True)
+  dateb=models.DateField(null=True)
+  datej=models.DateField(null=True)
+  department=models.CharField(max_length=150,null=True)
+  age=models.IntegerField(max_length=2,null=True)
+  address=models.CharField(max_length=300,null=True)
+  salary=models.IntegerField(max_length=10,null=True)
+  position=models.CharField(max_length=100,null=True)
+  
+  def __str__(self):
+     return self.empname
+  
+class login(models.Model):
+  uid=models.CharField(max_length=20)
+  password=models.CharField(max_length=20)
+
+
+  def __str__(self):
+     return self.uid
+  
+
+
+class manager(models.Model):
+  managerid=models.CharField(max_length=10,unique=True)
+  managername=models.CharField(max_length=100)
+  mdepartment=models.CharField(max_length=50)
+
+  def __str__(self):
+     return self.managerid
+
