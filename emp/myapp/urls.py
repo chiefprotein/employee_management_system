f78a8bd7f6232a57d9cff2296269e604053515f9
chiefprotein <jenilbiju@gmail.com>
2023-12-14 17:41:28 +0530

    emp

--- emp/myapp/urls.py
+++ emp/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.loginUser),
+    path('', views.loginUser, name= 'loginuser'),
     path('home/', views.home, name='home'),
     path('add_emp/', views.add_emp),
 ]
--- emp/myapp/views.py
+++ emp/myapp/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 def loginUser(request):
-    username = request.POST.get("username")
-    password = request.POST.get("password")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, ("Poyi marayadiku enter cheyyada myre.."))
+            return redirect('loginuser')
 
-    user = authenticate(request, username=username, password=password)
-
-    if user is not None:
-        login(request, user)
-        print(user)
-        return redirect('home')
-
-    return render(request, 'myapp/login.html')
+    else:
+        return render(request, 'myapp/login.html')
 def home(request):
     return HttpResponse('home')
 def add_emp(request):
