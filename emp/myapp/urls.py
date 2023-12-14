35a4cba19d5648a2b5bd1b397904f8633cf9f697
kiwi965 <kevinbenny2002@gmail.com>
2023-12-14 10:58:37 +0530

    emp

--- emp/myapp/urls.py
+++ emp/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login),
-    path('home/', views.home),
+    path('', views.loginUser),
+    path('home/', views.home, name='home'),
     path('add_emp/', views.add_emp),
 ]
--- emp/myapp/views.py
+++ emp/myapp/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
-def login(request):
+def loginUser(request):
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        print(user)
+        return redirect('home')
+
     return render(request, 'myapp/login.html')
 def home(request):
     return HttpResponse('home')
